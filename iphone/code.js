b450a2a4da0518d23000eab71311d43c4619d87e
Tam250399 <tamthanhsk25@gmail.com>
2020-12-24 08:35:22 +0700

    add iphone

---
+++ iphone/code.js
@@ -0,0 +1,9 @@
+function Iphone3() {
+    let b = document.getElementById("abc").value
+    document.getElementById("xyz").value = b;
+}
+
+function Samsung3() {
+    let c = document.getElementById("xyz").value
+    document.getElementById("abc").value = c;
+}
